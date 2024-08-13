de5905ab1a7e8067c0fad0713f60adbb26309126
H-Multani <82038303+H-Multani@users.noreply.github.com>
2024-08-14 02:29:25 +0530

    Time: 38 ms (47.48%), Space: 44.6 MB (85.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,36 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int len=nums.length;
-        int i=0,j=len-1;
-        int[] temp=nums.clone();
-        Arrays.sort(temp);
-        while(i<j){
-            int sum=temp[i]+temp[j];
-            if(sum==target){
-                break;
-            }
-            else if (sum<target){
-                // increment sum ke liye move i ahead
-                i++;
-            }
-            else j--;
-        }
+        int[] ans;
 
-        int value1=temp[i];
-        int value2=temp[j];
-
-        // search for the values in the original array and store in array, the indexes
-        int index1=-1,index2=-1;
-        for(i=0;i<len;i++){
-            if(nums[i]==value1 && index1==-1){
-                index1=i;
-            }
-            else if(nums[i]==value2 && index2==-1){
-                index2=i;
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+                if(nums[i]+nums[j]==target){
+                    ans=new int[]{i,j};
+                    return ans;
+                }
             }
         }
-
-        int[] ans={index1,index2};
+        ans=new int[2];
         return ans;
 
     }
