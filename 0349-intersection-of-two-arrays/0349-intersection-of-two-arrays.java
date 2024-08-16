d267156da24494545f8677ae0c96368278551d98
H-Multani <82038303+H-Multani@users.noreply.github.com>
2024-08-17 01:23:12 +0530

    Time: 17 ms (6.67%), Space: 43.4 MB (21.57%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,48 +1,25 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> ans=new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         int[] finalans;
-        if(nums1.length<nums2.length){
-            ArrayList<Integer> num=new ArrayList<>();
+        ArrayList<Integer> num = new ArrayList<>();
 
-            for(int it:nums2){
-                if(!num.contains(it)){
-                    num.add(it);
-                }
-            }
-
-            for(int i=0;i<nums1.length;i++){
-                if(!ans.contains(nums1[i]) && num.contains(nums1[i])){
-                    ans.add(nums1[i]);
-                }
-            }
-
-            finalans=new int[ans.size()];
-
-            for(int i=0;i<ans.size();i++){
-                finalans[i]=ans.get(i);
+        for (int it : nums2) {
+            if (!num.contains(it)) {
+                num.add(it);
             }
         }
-        else{
-            ArrayList<Integer> num=new ArrayList<>();
 
-            for(int it:nums1){
-                if(!num.contains(it)){
-                    num.add(it);
-                }
+        for (int i = 0; i < nums1.length; i++) {
+            if (!ans.contains(nums1[i]) && num.contains(nums1[i])) {
+                ans.add(nums1[i]);
             }
+        }
 
-            for(int i=0;i<nums2.length;i++){
-                if(!ans.contains(nums2[i]) && num.contains(nums2[i])){
-                    ans.add(nums2[i]);
-                }
-            }
+        finalans = new int[ans.size()];
 
-            finalans=new int[ans.size()];
-
-            for(int i=0;i<ans.size();i++){
-                finalans[i]=ans.get(i);
-            }
+        for (int i = 0; i < ans.size(); i++) {
+            finalans[i] = ans.get(i);
         }
 
         return finalans;
