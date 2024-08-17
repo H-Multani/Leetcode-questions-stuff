41b4a8198860a137de19a7651c43a9363c5493ff
H-Multani <82038303+H-Multani@users.noreply.github.com>
2024-08-17 11:53:31 +0530

    Time: 1 ms (98.61%), Space: 41.7 MB (89.73%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,32 +1,35 @@
 class Solution {
     public boolean isValid(String word) {
-        char[] wordnew = word.toCharArray();
-        
-        boolean isvowel = false;
-        boolean isconsonant = false;
-        int charcnt=0;
-        int len = wordnew.length;
+        int len=word.length();
         if(len<3) return false;
-        
-        for (int i = 0; i < len; i++) {
+        boolean isvowel=false,isconsonant=false;
+        for(int i=0;i<len;i++){
+            char curr=word.charAt(i);
 
-            if (wordnew[i] == '@' || wordnew[i] == '#' || wordnew[i] == '$') {
-                return false;
+            if(curr=='#' || curr=='@' || curr=='$') return false;
+
+            // matlab abhi tk special char nai mile hai
+
+            // number hai kya??
+            if(curr>='0' && curr<='9'){
+                // number hai
             }
-            else if (wordnew[i] <= '9' && wordnew[i] >= '0') {
-                // isnumber = true;
+            // vowel hai kya??
+            else if(curr=='a' ||curr=='e' ||curr=='i' || curr=='o' || curr=='u' ||curr=='A' ||curr=='E' || curr=='I' ||curr=='O' ||curr=='U'){
+                // vowel hai
+                isvowel=true;
             }
-            else if (wordnew[i] == 'a' || wordnew[i] == 'e' || wordnew[i] == 'i' || wordnew[i] == 'o' || wordnew[i] == 'u'
-                    || wordnew[i] == 'A' || wordnew[i] == 'E' || wordnew[i] == 'I' || wordnew[i] == 'O'
-                    || wordnew[i] == 'U') {
-                isvowel = true;
+            else{
+                // consonant hai only
+                // if me number chk karaya->false, nai hai number
+                // else if me vowel chk karaya->false, vowel nai hai
+                // else me,consonant hi hoga
+                isconsonant=true;
             }
-            else isconsonant=true;
         }
 
         if(isvowel && isconsonant) return true;
 
         return false;
-
     }
 }
