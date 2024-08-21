4aa8c35032408ec86e2cf8cd31e27f2d7a2a78d5
H-Multani <82038303+H-Multani@users.noreply.github.com>
2024-08-21 10:54:39 +0530

    Time: 0 ms (100%), Space: 42.3 MB (26.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,33 +1,51 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        ArrayList<Integer> num=new ArrayList<>(); 
         int i=0,j=0;
+        ArrayList<Integer> arr=new ArrayList<>();
+
+        // common loop
         while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                num.add(nums1[i]);
-                i++;
+            if(nums1[i]==nums2[j]){
+                arr.add(nums1[i]);
+                arr.add(nums2[j]);
+                i++;j++;
             }
-            else{
-                num.add(nums2[j]);
+            else if(nums1[i]>nums2[j]){
+                // j wala element chhota hai, add first
+                // since sorted order me chahiye
+                arr.add(nums2[j]);
                 j++;
             }
-        } 
+            else{
+                // here means, i wala element chhota hai, add thos first
+                arr.add(nums1[i]);
+                i++;
+            }
+        }
 
-        // in case nums2 end first , and nums1 ke m element nai liye hai abhi tak
+        // in case nums1 se m elemnts nai liye hai
         while(i<m){
-            num.add(nums1[i]);
-            i++;
+        // directly add the elements
+        // since nums1 ke elements already sorted order me hai
+        arr.add(nums1[i]);
+        i++;
         }
-        // in case nums1 end first,and nums2 ke n element nai liye hai abhi tak
+        // in case nums2 se n elemnts nai liye hai
         while(j<n){
-            num.add(nums2[j]);
-            j++;
+        // directly add the elements
+        // since nums2 ke elements already sorted order me hai
+        arr.add(nums2[j]);
+        j++;
         }
 
-        for(i=0;i<num.size();i++){
-            nums1[i]=num.get(i);
+        for(i=0;i<m+n;i++){
+            nums1[i]=arr.get(i);
         }
+        // arraycopy() can be used
 
         return;
+
+
+
     }
 }
