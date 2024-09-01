0df74fc6d5fc46c91ae8a61630ea09c3c7078c4e
H-Multani <82038303+H-Multani@users.noreply.github.com>
2024-09-02 03:04:08 +0530

    Time: 77 ms (73.39%), Space: 91.4 MB (50.82%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -3,23 +3,23 @@ public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         vector<vector<int>> ans;
         if(original.size()!=m*n) return ans;
-        int curr_column=0;
+        int curr_row_idx=0;
         vector<int> temp;
         for(auto it:original){
-            if(curr_column<n){
+            if(curr_row_idx<n){
                 temp.push_back(it);
-                curr_column++;
+                curr_row_idx++;
             }
-            else if(curr_column==n){
+            else if(curr_row_idx==n){
                 // curr row filled, push to ans
                 ans.push_back(temp);
                 // clear the temp for elements of next row, and reset the column counter
                 temp.clear();
-                curr_column=0;
+                curr_row_idx=0;
 
                 // now add current element to new row
                 temp.push_back(it);
-                curr_column++;
+                curr_row_idx++;
             }
         }
         ans.push_back(temp);
