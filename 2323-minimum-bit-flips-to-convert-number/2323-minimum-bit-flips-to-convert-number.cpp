b1361a5d0945fa0923fd646808d4f32e58980842
H-Multani <82038303+H-Multani@users.noreply.github.com>
2024-09-11 12:17:14 +0530

    Time: 0 ms (100%), Space: 7.4 MB (93.51%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
-    string decToBinary(int n) {
-        string ans = "";
-        // Size of an integer is assumed to be 32 bits
-        for (int i = 31; i >= 0; i--) {
-            int k = n >> i;
-            if (k & 1)
-                // cout << "1";
-                ans += '1';
-            else
-                // cout << "0";
-                ans += '0';
-        }
-        return ans;
-    }
     int minBitFlips(int start, int goal) {
-        string st = decToBinary(start);
-        string end = decToBinary(goal);
-        int overallsize = max(st.size(), end.size());
-        // while(st.size()<overallsize) st='0'+st;
-        // while(end.size()<overallsize) end='0'+end;
-        cout << st << " " << end << endl;
-        int cnt = 0;
-        for (auto i = 0; i < st.size(); i++) {
-            if (st[i] != end[i])
-                cnt++;
-        }
-        return cnt;
+        // approach
+        // iss ko dry dun karke dekh lena, toh acche se samajh aaega
+        // dono nos ka xor lelo, agar different honge bits toh xor me 1 aa jaega
+        // un values(indexes) ke liye
+        int ans;
+        ans = start ^ goal;
+        // now jitne bits change karne hai utne 1 apne xor(ans) me hai, so for
+        // final ans just count the number of 1s in the xor
+        return __builtin_popcount(ans);
     }
 };
