23c446c348ce498d2bc96f7010995b84ac8615c9
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2024-09-22 12:47:01 +0530

    Time: 157 ms (10.94%), Space: 73.8 MB (14.89%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_map<int,pair<int,int>> mpp; // stores node and degree(in and out)
-        if(n==1) return 1;
-        for(auto it:trust){
+        unordered_map<int, pair<int, int>>
+            mpp; // stores node and degree(indegree and outdegree)
+        // in the pair, first is how many ppl trust you cnt
+        // in the pair, second is how many ppl you trust cnt
+        // we need ans ke pair ka first to be ==n-1, ie everyone trusts us
+        // and ans ke pair ka second to be ==0, ie we trust nobody
+        if (n == 1)
+            return 1;
+        for (auto it : trust) {
+            // it[0] trusts it[1]
+            // hence it[0] ka pair ka second increment by 1
             mpp[it[0]].second++;
+            // it[1] is trueted by 1 more person
+            // and it[1] ka pair ka first increment by 1
             mpp[it[1]].first++;
             // if(mpp.find(it[1])==mpp.end()) mpp[it[1]]=0;
         }
-        
-        for(auto it:mpp){
-            if(it.second.second==0 && it.second.first==n-1) return it.first;
+
+        for (auto it : mpp) {
+            // it.second is pair
+            // it.second.second is stuff inside the pair
+
+            // we need ans ke pair ka first to be ==n-1, ie everyone trusts us
+            // and ans ke pair ka second to be ==0, ie we trust nobody
+            if (it.second.second == 0 && it.second.first == n - 1)
+                return it.first;
         }
 
         return -1;
