8cf10ba88655354eebd738a7ae782bd0ef8e1c2c
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2024-09-28 02:43:09 +0530

    Time: 68 ms (91.59%), Space: 37.8 MB (95.94%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -36,33 +36,13 @@ public:
                 // in this region 2 events are overlapping(causing double
                 // overlap), push the region to overlaps
 
-                //  no need to check this, since curr region (start,end) tk ke
-                //  liye already chk kar liye hai, if triple overlaps ho rha ki
-                //  nai, no need to chk again
-                
-                // before pushing the current region to overlaps, check if the
-                // region is already overlapping with some other region in the
-                // overlaps array, since if it does, that will mean that the
-                // current region already overlaps with some region jaha 2
-                // events hai pehle se, toh in that case return false straight
-                // away
-                pair<int, int> currregion;
-                currregion.first = max(start, currstart);
-                currregion.second = min(end, currend);
-
-                // // chk if curent region is already overlapping with some
-                // region for (auto it2 : overlaps) {
-                //     if (max(currregion.first, it2.first) <
-                //         min(currregion.second, it2.second)) {
-                //         // means triple overlap occuring here
-                //         // means this event cant be placed, return false
-                //         return false;
-                //     }
-                // }
-                // we here
-                // means no triple overlaps found, push the current 2 overlap
-                // region to overlaps array(since in this region, 2 events
-                // overlap)
+                //  no need to check for triple overlap, since curr region
+                //  (start,end) tk ke liye already chk kar liye hai, if triple
+                //  overlaps ho rha ki nai, no need to chk again
+                pair<int,int> currregion;
+                currregion.first=max(start,currstart);
+                currregion.second=min(end,currend);
+                // directly push the 2 overlap region to 
                 overlaps.push_back(currregion);
             }
         }
