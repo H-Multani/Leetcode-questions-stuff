e5c528b6d6e6686c719f63d7cd2fd08c4b04b6b2
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2024-10-01 12:15:45 +0530

    Time: 119 ms (68.39%), Space: 73.6 MB (63.09%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
         unordered_map<int, int> remainders;
-        cout << (-4) % 3 << endl;
         for (auto it : arr) {
             int rem = ((it % k) + k) % k;
             // for cases if arr[i] is negative, this is the way to get remainder
@@ -18,13 +17,13 @@ public:
         }
 
         for (auto i = 0; i < arr.size(); i++) {
-            // curr ka remainder= arr[i]%k
-            // iska pair banane ke iye remainder needed= k- (curr ka remainder)
+            // curr ka remainder= ((arr[i]%k)+k)%k
+            // iska pair banane ke liye remainder needed= k- (curr ka remainder)
             // since pair tabhi banega if (arr[i]+arr[j])%k==0, aur ye tab hoga
             // if (arr[i]%k)+(arr[j]%k)==k, remainder 0 dene waalo ke liye case
             // alag banega bas
             int remainder_first_half = ((arr[i] % k) + k) % k;
-            // incase arr[i] is negative
+            // incase arr[i] is negative this is the remainder, also works for +ve values
             int remainder_other_half = k - remainder_first_half;
             // cout << arr[i] % k << " " << remainder_pair_key << endl;
 
@@ -35,9 +34,9 @@ public:
                 continue;
 
             // now we have 2 keys for the remainders map
-            // first key is arr[i]%k and second key is remainder_pair_key
+            // first key is remainder_first_half and second key is remainder_other_half
             // agar dono key ka value same hua means arr[i] wale element ke liye
-            // pair ban sakta hai, in such case move to chk the next pair,
+            // pair ban sakta hai, in such case move to chk pair for the next value
             // otherwise if value not same means arr[i] wale element ke liye
             // pair nai ban paega, in such case return false
             if (remainders[remainder_first_half] !=
