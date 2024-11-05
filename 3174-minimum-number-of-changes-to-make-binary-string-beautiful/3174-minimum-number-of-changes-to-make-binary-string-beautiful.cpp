6005e421776e5b7c6dea3ad47e84a586bc85bb83
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2024-11-05 16:43:46 +0530

    Time: 0 ms (100%), Space: 12.3 MB (16.88%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -7,19 +7,29 @@ public:
         // chk karo kya usme flip required hai
 
         // toh har 2 size ke substring ko chk karo, if sahi hai(ie both 0 or
-        // both 1) toh kuch nai karo, otherwise 1 operation(flip) karke sahi kar do
+        // both 1) toh kuch nai karo, otherwise 1 operation(flip) karke sahi kar
+        // do
 
         // har 2 size ka substring will be current char and uska adjacent char,
         // here we will take current char and uske aage wala adjacent character
-        
+
         int n = s.size();
 
         int ans = 0;
         for (int i = 0; i < n - 1; i += 2) {
+            // the substring of size 2 is s[i] and s[i+1] wale characters
+
+            // chk if the substring is valid or not
             if (s[i] != s[i + 1]) {
-                // same nai hai dono, flip required
+                // we here means substring like "01" or "10", not valid
+
+                // same nai hai dono characters, ie substring not valid, flip
+                // required, kar do flip koi bhi 1 character ko
                 ans++;
             }
+
+            // i+2 to move directly to the next substring
+            
         }
 
         return ans;
