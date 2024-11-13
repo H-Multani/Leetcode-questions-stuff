02a71f0a46695f6066b3ca92e5c085c119ffe98a
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2024-11-14 04:38:41 +0530

    Time: 68 ms (41.04%), Space: 60.4 MB (61.15%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -6,10 +6,14 @@ public:
         long long x=0,y=0;
 
         for(auto i=0;i<nums.size();i++){
+            // since starting frm i+1 th element hence nums.begin()+i+1
             long long index_x=lower_bound(nums.begin()+i+1,nums.end(),lower-nums[i])-nums.begin();
+            // beech ke elements ka count
             long long cntx=index_x-i-1;
             x+=cntx;
+            // since starting frm i+1 th element hence nums.begin()+i+1
             long long index_y=upper_bound(nums.begin()+i+1,nums.end(),upper-nums[i])-nums.begin();
+            // beech ke elements ka count
             long long cnty=index_y-i-1;
             y+=cnty;
         }
