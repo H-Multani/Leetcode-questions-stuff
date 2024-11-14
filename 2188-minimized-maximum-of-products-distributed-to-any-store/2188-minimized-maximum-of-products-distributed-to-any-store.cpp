26c92003e33411b1b793c593ea34f0006f685444
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2024-11-15 05:01:16 +0530

    Time: 30 ms (81.25%), Space: 87.3 MB (72.39%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     // checks agar har bande ko at max mid jitne produce denge toh kya all
-    // products consumed
+    // items consumed???
     bool isvalid(int n, int val, vector<int>& quantities) {
         int shopsfilled = 0;
         for (auto it : quantities) {
-            // current type has 'it' no of products, har shop ko val jitne
+            // current item has 'it' no of products, har shop ko val jitne
             // products do
             if (it % val == 0) {
                 // means har shop ko val jitne products milenge
+                // eg if val=5 and it=15, products split to shop like {5,5,5},
+                // ie each shop gets 5 products and total shops filled=it/val=3
                 shopsfilled += it / val;
             } else {
                 // means har shop ko val jitne products milenge except one shop,
                 // aakhri wali shop ko <val products milenge, lekin phir bhi ye
                 // shop filled me count karegi, since we can give at max one
                 // type of item to each shop
+
+                // eg if val=5 and it=12, products split to shop like {5,5,5,2},
+                // ie each shop gets 5 products(except last shop) and total
+                // shops filled=(it/val)+1=3+1=4
                 shopsfilled += (it / val) + 1;
             }
 
             // if at any point shops filled>n, means zyada shops bhar di hai,
+            // items will be left undistributed
+
             // product not distributed, in such case return false
             if (shopsfilled > n)
                 return false;
@@ -26,8 +34,10 @@ public:
 
         // we here means saara product(and items) finish ho chuke hai,(even if
         // with some shops left with 0 product which is also valid)
-        // since no product left hence we can fill all shops with har shop
-        // getting at max val jitne products
+
+        // since no product left hence we can fill all shops with each shop
+        // getting at max 'val' jitne products
+        
         // hence return true, since no product left
         return true;
     }
@@ -47,32 +57,33 @@ public:
             // kya
             // eg mid=5, chk if har store ko 5(or less) product de agar toh kya
             // saare products distribute ho jaenge??
+            // it at max 5 product per shop
 
             // like in eg1, if mid=5,
             // distribute 11 like 5,5,1(since ek store ko ek product type hi de
-            // sakte hai ) , ttl stores done=3
-            // distribute 6 like 5,1(since ek store ko ek product type hi de
-            // sakte hai) ttl stores done=2 overall stores filled=3+2=5,
-            // stores filled like {5,5,1,5,1,0}(0 filled wala bhi valid hai accd
-            // to question)
-            // now all stores are filled no problem, and no product remaining
-            // out, so this mid is a possible ans, since possible hai mid jitne
-            // parts karke fill karna in such case maxm value of profuct
-            // recieved by any store=5,
-            // ab isko minimize karna hai toh lets try with mid=2
-            // mid=2
-            // distribute 11 like 2,2,2,2,2,1(since ek store ko ek product type
-            // hi de sakte hai ) , ttl stores done=6 stores filled like
-            // {2,2,2,2,2,1} but abhi toh product 6 bacha hai for distribution,
-            // and already stores have been filled, that means all product not
-            // distributed, this is not a possible ans for our question, hence
-            // increase mid value and try again,
-            // yehi upar wala karna hai basically
+            // sakte hai) , ttl stores done=3
+            // distribute next item-> 6 like 5,1(since ek store ko ek product
+            // type hi de sakte hai) ttl stores done=2 overall stores
+            // filled=3+2=5, stores filled like {5,5,1,5,1,0}(0 filled wala bhi
+            // valid hai accd to question)
+            // now all stores are filled no problem,
+            // and no product remaining out, so this mid is a possible ans,
+            // since possible hai mid jitne parts karke fill karna in such case
+            // maxm value of product recieved by any store=5, ab isko minimize
+            // karna hai toh lets try with mid=2
+
+            // mid=2 distribute 11 like
+            // 2,2,2,2,2,1(since ek store ko ek product type hi de sakte hai ) ,
+            // ttl stores done=6 stores filled like {2,2,2,2,2,1} but abhi toh
+            // item ->6 bacha hai for distribution, and already stores have
+            // been filled, that means all product not distributed, this is not
+            // a possible ans for our question, hence increase mid value and try
+            // again, yehi karna hai basically
 
             if (isvalid(n, mid, quantities)) {
                 // means ye wala mid value lenge toh all product will be
                 // distributed no problem,in such case apan har shop ko at max
-                // mid jitne product de rahe hai, so this mid is a possible
+                // 'mid' jitne product de rahe hai, so this 'mid' is a possible
                 // answer, toh update kardo
                 ans = mid;
 
