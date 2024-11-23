f59c427685a474b4913894cd31c98f3fe5b8169a
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2024-11-24 04:43:59 +0530

    Time: 200 ms (33.88%), Space: 56.4 MB (25.31%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -25,16 +25,20 @@ public:
         // har ccolumn ke neeche jao, waha se iterate karte akrte swap karte jao
         for (int col = 0; col < n; col++) {
             int stoneidx = m - 1, emptyidx = m - 1;
+
+            // current row par iterate karo, neeche se
             for (int row = m - 1; row >= 0; row--) {
                 if (ans[row][col] == '#') {
                     // means yaha stone hai, swap stone with empty idx wala
                     // space
                     swap(ans[stoneidx][col], ans[emptyidx][col]);
+                    // ab ye stone gravity se apni correct posn par hai
 
                     // after swapping, ab previously empty space par stone aa
                     // jaega, toh emptyidx aage badhao, to get emptyidx to the
                     // next empty space, also ab previously stone wale space par
                     // empty ho jaega, toh stoneidx bhi aage badha do
+
                     // basically dono idx ko aage badha do
 
                     emptyidx--;
@@ -44,8 +48,8 @@ public:
 
                     // koi bhi iske upar aane wala stone iss obstacle ke neeche
                     // nai jaayega, ie agar stone neeche aaya toh iss obstacle
-                    // ke upar rahega, hence stoneidx ko just stone ke upar wale
-                    // idx par rakh do empty idx ko curr location se 1 opar rakh
+                    // ke upar rahega, hence emptyidx ko just obstacle ke upar wale
+                    // idx par rakh do , ie emptyidx ko curr location se 1 upar rakh
                     // do
                     emptyidx = row - 1;
 
@@ -56,6 +60,8 @@ public:
                     // ie yaha empty space mili hai
                     // now we dont set emptyidx to this posn, since emptyidx is
                     // already at empty index nearest to bottom
+
+                    
                     // yaha stone nai hai, stoneidx aage badhao
                     stoneidx--;
                 }
