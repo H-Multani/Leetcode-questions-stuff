157bc442cf5b632c1cb610f06efe1993862d47cc
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2024-12-03 17:22:47 +0530

    Time: 19 ms (63.55%), Space: 85.6 MB (16.63%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -3,12 +3,18 @@ public:
     string addSpaces(string s, vector<int>& spaces) {
         string ans="";
         int j=0;
+        // for spaces array
         for(int i=0;i<s.size();i++){
             if(j<spaces.size() && i==spaces[j]){
+                // means iss index ke pehle space chahiye ans me
+
+                // hence add space
                 ans+=' ';
+
+                // move to next index that requires space
                 j++;
             }
-
+            // add current character frm s
             ans+=s[i];
         }
 
