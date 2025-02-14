d08156ba49d6a229faa10fa994ca7770f04bac5d
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-02-14 17:34:44 +0530

    Time: 20 ms (68.21%), Space: 77.8 MB (93.67%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,24 +1,73 @@
 class ProductOfNumbers {
-    // vector bana do store karlo
-    vector<int> stuff;
+    // prefix sum use karenge,
+    vector<int> prefix;
+
 public:
     ProductOfNumbers() {
         // nothing to do here really
     }
-    
-    void add(int num) {
-        // new number add kardo array me
-        stuff.push_back(num);
-    }
-    
-    int getProduct(int k) {
-        // start frm n-k th posn, and products lete jao
-        int n=stuff.size();
-        int prod=1;
-        for(int i=n-1;i>=n-k;i--) prod*=stuff[i];
 
-        return prod;
+    void add(int num) {
+        // new number add kardo prefix list me
+        // 2 cheez hai yaha par ab
+
+        // thing 1
+        // agar number 0 hai toh yaha se kitna bhi peeche chale jao k leke, end
+        // me product 0 hi aayega
+        // like if prefix becomes like [2,4,12,0], toh ab yaha se k=1,2,3,4
+        // sabke liye product 0 hi hai toh iss case me peehche waale jinte
+        // numbers products the faltu ban jayenge, hence no use storing those
+        // now, clear the prefix array(hint 2 me bhi yehi likha hai)
+        if (num == 0) {
+            prefix.clear();
+            return;
+        }
+
+        // thing 2
+        // agar list empty hai prefix waali toh seedhe add kardo
+        if (prefix.empty()) {
+            prefix.push_back(num);
+            return;
+        }
+
+        // now agar list empty bhi nai hai and num 0 bhi nai hai toh last number
+        // leke multiply krke add kardo
+        // basically prefix[n-1]*num karke add kardo prefix me
+        prefix.push_back(prefix[prefix.size() - 1] * num);
+    }
+
+    int getProduct(int k) {
+
+        // ab yaha 2 case hai either k<n or k>n
+
+        // agar k>n means peeche kahi 0 aaya hoga uss time apan ne array clear
+        // kar diya hoga, toh vo 0 agar hota yaha toh overall product 0 hi aata
+        // numbers ka since questn always says ki elements always rahenge
+
+        // uss case me 0 return krdo
+        if (prefix.size() < k)
+            return 0;
+
+        // otherwise, product of last k elements is prefix[n-1]/prefix[n-k-1],
+        // since prefix[n-k-1] is product of all elements frm start upto n-k-1
+        // th idx, and prefix[n-1] is product of all elements frm start to n-1
+        // th idx. toh product of only k elements frm end will be product of all
+        // frm 0 to n-1 / product of all frm 0 to n-k-1
+        // isko dry run karna aa jayega samajh me 
+        int n=prefix.size();
+
+        // edge case, if n==k, simply return prefix[n-1]
+        if(n==k) return prefix[n-1];
+
+        // for(auto it:prefix){
+        //     cout<<it<<" ";
+        // }
+        // cout<<endl<<k<<" "<<endl;
+        // cout<<prefix[n-1]<<" "<<prefix[n-k-1]<<endl;
+        // cout<<endl;
+
         
+        return prefix[n-1]/prefix[n-k-1];
     }
 };
 
