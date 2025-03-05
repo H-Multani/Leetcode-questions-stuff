c279f505db4ff9b7f8803a3e82a60c92c99d124d
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-03-05 15:19:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     long long coloredCells(int oldn) {
+        long long n = oldn;
         // jiase jaise n increase hora hai, some things are happening
 
         // 1. beech wali line is increasing like 1,3,5,7,9...., ie odd numbers
         // me ie for n=2->colorcells=3,for n=3->colorcells=5,for
         // n=4->colorcells=7 and so on
         // formula for this line is (2*n)-1
-        long long n=oldn;
         // toh sabse pehle toh ye middle line ka add karo
         long long ans = 0;
         ans += (2 * n) - 1;
@@ -32,6 +32,19 @@ public:
         // ab saare count kar liye hai coloured boxes, beech wale line me honge
         // (2n-1) colured boxes and upar and neeche side rahenge (n-1)^2 boxes
         // each side, hence 2 baar laga diya, ab return krdo ans
+
+        // ye pura maine notice kiya hai, logic laga kar, beech wala line ka
+        // samajh aa gaya tha lekin upar and neeche wale side ka logic aise
+        // dhunda,
+
+        // as n increases, upar wale side me lines increase like 1,3,5,7,9.... ,
+        // ie for n=2->lines=1
+        // ie for n=3->lines=1,3
+        // ie for n=4->lines=1,3,5...
+
+        // toh we find sum of n-1 odd natural numbers, which is (n-1)^2, hence
+        // upar wale side me (n-1)^2 jitne colured squares honge total, similar
+        // for neeche wala side
         return ans;
     }
 };
