70d920e75331aa3c0bc053dc3b2084e04584d736
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-03-09 01:49:37 +0530

    Time: 7 ms (3.63%), Space: 8.4 MB (31.05%) - LeetHub

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        // sliding window, hint padha tab palle pada
+
+        // hints jo bolra vhi kardo, k size ka har possible substring lelo, har
+        // substring me kitne colour karne padenge count karlo, jo bhi minm
+        // count ho return kardo
+
+        // start from left most side at idx 0
+        int left = 0, right = k;
+        int ans = INT_MAX, n = blocks.size();
+
+        while (right <= n) {
+            // current block utha kar check karo ki kitne baar colour karna
+            // padega
+
+            // basically agar current substring liya toh kitne blocks ko colour
+            // karna padega to make this (left,right) substring an occurence of
+            // k consecutive black blocks
+
+            // basically agar current substring ko k consecutive black wala
+            // banana hai toh kitne blocks colour karne padenge apan ko
+
+            int cnt = 0;
+
+            for (int i = left; i < right; i++) {
+                // agar white hai curent wala toh colour karna padega, count
+                // karlo
+                if (blocks[i]=='W') cnt++;
+            }
+            cout<<cnt<<endl;
+
+            // ans update kardo, minm count chahiye apan ko
+            ans=min(ans,cnt);
+
+            // windo aage badha do
+            left++;right++;
+        }
+
+        // ab jo bhi ans aaya return kardo
+        return ans;
+    }
+};
