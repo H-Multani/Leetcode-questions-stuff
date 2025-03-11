42ce70e00ba2fe79cd32c8f17d2afd7ab90a8593
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-03-12 04:16:00 +0530

    Time: 46 ms (6.46%), Space: 16.9 MB (8.97%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,27 +1,9 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // similar logic as lc 3306, insted of next consonant, we store next a,
-        // next b, next c, and sliding windo as always
-
-        int n = s.size();
-        vector<int> nexta(n), nextb(n), nextc(n);
-        int lasta = n, lastb = n, lastc = n;
-
-        for (int i = n - 1; i >= 0; i--) {
-            nexta[i] = lasta;
-            nextb[i] = lastb;
-            nextc[i] = lastc;
-
-            // char update kardo
-            if (s[i] == 'a')
-                lasta = i;
-            if (s[i] == 'b')
-                lastb = i;
-            if (s[i] == 'c')
-                lastc = i;
-        }
-
+        // similar logic as lc 3306, sliding windo as always
+        // lekin vo next a,b,c wala vector nai lagega
+        int n=s.size();
         // sb sliding window chala do
         int l = 0, r = 0, ans = 0;
 
@@ -40,26 +22,18 @@ public:
                 // since we will be here as soon as the windo becomes valid, toh
                 // r wale se hi chalu karenge similar to lc 3306
 
-                // if (s[r] == 'a') {
-                //     // agar 'a' hai toh next a wale array se add karenge
-                //     ans += nexta[r] - r;
-                // }
-                // if (s[r] == 'b') {
-                //     // agar 'b' hai toh next b wale array se add karenge
-                //     ans += nextb[r] - r;
-                // }
-                // if (s[r] == 'c') {
-                //     // agar 'c' hai toh next c wale array se add karenge
-                //     ans += nextc[r] - r;
-                // }
-                ans+=n-r;
-                // cout<<s.substr(l,r-l+1)<<" "<<ans<<endl;
+                // since we here when window valid, toh ab iss r ke aage jitne
+                // char lagaoge saare valid substring banaenge, toh count karlo
+
+                ans += n - r;
+
+                // all these n-r substring all start frm same L
 
                 // ab iss l se chalu hone waale saare valid windows count kar
                 // liye hai apan ne, hence l wale char ko window se nikal do, ie
                 // shrink windo to move to next l
                 mpp[s[l]]--;
-                if(mpp[s[l]]==0){
+                if (mpp[s[l]] == 0) {
                     // agar freq 0 pahuch gai toh map se alag kar do
                     mpp.erase(s[l]);
                 }
