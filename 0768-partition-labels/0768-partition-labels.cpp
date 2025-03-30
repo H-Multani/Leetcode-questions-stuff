9d1e1c63403d4ac39f44fd8aa564b257e34b6d44
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-03-31 00:45:14 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.22%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -10,20 +10,22 @@ public:
         // for eg 2 b={3,6},c={1,9},d={7,7},e={0,8}
         // ab agar s par iterate kare apan toh
         // at i=0 we have e, e ka last idx is 8, toh current partition idx 8 tk
-        // jayega, ab apan j=1 se 8 tak chalaenge, agar beech me koi aur char
-        // mila jiska jiska last idx>8 hai toh ye wala partition expand hoga aur
-        // j=1, c hai, c ka ending idx is 9, toh ye partition 8 par nai 9 par
-        // khatam hoga, so that each char appears in only 1 part
-        // j=2, me c hai, last char at idx 9, current ending is idx 9
-        // j=3, me b hai, last char at idx 6, current ending is idx 9
-        // j=4, me b hai, last char at idx 6, current ending is idx 9
-        // j=5, me b hai, last char at idx 6, current ending is idx 9
-        // j=6, me b hai, last char at idx 6, current ending is idx 9
-        // j=7, me d hai, last char at idx 7, current ending is idx 9
-        // j=8, me e hai, last char at idx 8, current ending is idx 9
-        // j=9, me c hai, last char at idx 9, current ending is idx 9
-        // toh ye wala partition me 9-0+1=10 char honge, ans me daal kar aage
-        // badhenge starting frm i=10 now, repeat same process
+        // jayega, ab apan i=1 se 8 tak chalaenge, agar beech me koi aur char
+        // mila jiska jiska last idx>8 hai toh ye wala partition expand hoga
+        // i=1, c hai, c ka ending idx is 9, toh ye partition 8 par nai 9 par
+        // khatam hoga, so that curr char appears in only 1 partition(valid
+        // partition banane vaaste)
+        // i=2, me c hai, last char at idx 9, current ending is idx 9
+        // i=3, me b hai, last char at idx 6, current ending is idx 9
+        // i=4, me b hai, last char at idx 6, current ending is idx 9
+        // i=5, me b hai, last char at idx 6, current ending is idx 9
+        // i=6, me b hai, last char at idx 6, current ending is idx 9
+        // i=7, me d hai, last char at idx 7, current ending is idx 9
+        // i=8, me e hai, last char at idx 8, current ending is idx 9
+        // i=9, me c hai, last char at idx 9, current ending is idx 9
+        // toh ye wala partition me 9-0+1=10 char honge, ans me
+        // daal kar aage badhenge starting new partition frm i=10 now, repeat
+        // same process
 
         vector<pair<int, int>> idx(26, {INT_MAX, -1});
 
@@ -43,6 +45,8 @@ public:
         // current partition ka end, for starting this will be s ke 1st char ka
         // ending
         int currend = idx[s[0] - 'a'].second;
+
+        
         while (i < n) {
             // agar i currend par pahuch gaya hai means current wala partition
             // ban chuka hai
@@ -73,7 +77,7 @@ public:
             // do, this is so that, current character(jo abhi wale partition me
             // hai) uska last instance bhi current partition me hi aa jaaye, so
             // that overall partition is valid
-            currend=max(currend,idx[s[i]-'a'].second);
+            currend = max(currend, idx[s[i] - 'a'].second);
 
             // now move to the next char in current partition
             i++;
