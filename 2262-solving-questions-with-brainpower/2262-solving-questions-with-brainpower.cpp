1101cc7d554b3403066444cf509de6b7d347aa5c
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-04-02 04:53:08 +0530

    Time: 11 ms (32.9%), Space: 127.6 MB (27.42%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -7,13 +7,13 @@ public:
 
         // 2 options hai, take nottake
 
-        // agar memo me ans hai toh daal do
+        // agar memo me ans hai toh return krdo
         if(memo[i]!=-1) return memo[i];
 
         // take, curr i th bande ka points add kardo and move to
         // i+brainpower[i]+1 th idx, since i wala lene ke baad brainpower[i]
         // jitne nai le payenge
-        long long take =ques[i][0]+dosmth(i+ques[i][1]+1,ques,memo);
+        long long take = ques[i][0] + dosmth(i+ques[i][1]+1,ques,memo);
 
         // not take, i+1 th bande par chale jao
         long long nottake=dosmth(i+1,ques,memo);
@@ -29,7 +29,7 @@ public:
         // recurion and memo wala soln banao pehle toh
 
         // tle aara hai, meoize kardo, since only 1 moving part ie i, toh 1d memo se kaam ho jayega
-        vector<long long> memo(questions.size()+1,-1);
+        vector<long long> memo(questions.size(),-1);
 
         // funcn me send karenge apan questions, i(initially 0), currsum
         return dosmth(0, questions,memo);
