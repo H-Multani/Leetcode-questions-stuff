caf20cf4447c7a80170def0e71a5e6eb60d6947d
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-05-09 02:56:00 +0530

    Time: 2027 ms (5.1%), Space: 567.6 MB (5.09%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
@@ -5,7 +5,8 @@ public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
         // lc 3341 ka code
 
-        // pura code same as lc 3341, bss step count (1 or 2) wala logic ka explanation likha maine
+        // pura code same as lc 3341, bss step count (1 or 2) wala logic ka
+        // explanation likha maine
 
         int n, m;
         n = moveTime.size();
@@ -24,8 +25,10 @@ public:
 
             pq.pop();
 
-            // jab first time (n-1,m-1) par pahuchenge it will be smallest time, yehi se return krdo ans ko(mik probably)
-            if(currx==n-1 && curry==m-1) return currtime;
+            // jab first time (n-1,m-1) par pahuchenge it will be smallest time,
+            // yehi se return krdo ans ko(mik probably)
+            if (currx == n - 1 && curry == m - 1)
+                return currtime;
 
             if (time[currx][curry] < currtime)
                 continue;
@@ -50,9 +53,11 @@ public:
                     // ja rahe hai apan, toh jis new neighbour par jaa rahe hai
                     // uske i+j ke hisaab se steps count honge
 
-                    int steps ;
-                    if((newx+newy)%2==0) steps=2;
-                    else steps=1;
+                    int steps;
+                    if ((newx + newy) % 2 == 0)
+                        steps = 2;
+                    else
+                        steps = 1;
                     // foolproof formula, if i+j is even, we need 1 sec wala
                     // count and if i+j is odd we need 2 sec wala vahi kiya hai,
                     // by this steps me apne aap count ho jaega kitne sec wala
@@ -60,8 +65,8 @@ public:
                     // steps daal denge
 
                     if (time[newx][newy] == INT_MAX ||
-                        time[newx][newy] >
-                            max(currtime + steps, moveTime[newx][newy] + steps)) {
+                        time[newx][newy] > max(currtime + steps,
+                                               moveTime[newx][newy] + steps)) {
 
                         time[newx][newy] =
                             max(currtime + steps, moveTime[newx][newy] + steps);
