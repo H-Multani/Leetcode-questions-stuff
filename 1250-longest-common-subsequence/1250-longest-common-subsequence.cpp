20e2cb697b4f9b2f1717e32da6aaa695f53111bd
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-06-12 23:39:54 +0530

    Time: 28 ms (41.45%), Space: 27.4 MB (50.94%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,38 +1,53 @@
 class Solution {
 public:
-    int m,n;
-    // memo banao of size 1001 since ip is like that
-    int solve(string& s1, string& s2, int i,int j,vector<vector<int>>& memo){
-        if(i>=m || j>=n) return 0;
+    int longestCommonSubsequence(string s1, string s2) {
+        int m = s1.size();
+        int n = s2.size();
 
-        // agar memo me h toh bhej do
-        if(memo[i][j]!=-1) return memo[i][j];
+        vector<vector<int>> t(m + 1, vector<int>(n + 1));
 
-        // agar i th and j th char same hai toh dono ko aage badhao
-        if(s1[i]==s2[j]){
+        // t[i][j] means ki s1 of size i and s2 of size j ka LCA kya hai
 
-            // memo me update karke bhejna
-            memo[i][j]=1+solve(s1,s2,i+1,j+1,memo);
-            return memo[i][j];
+        // toh by this method overall ans will be t[m][n], ie LCA of s1 of size
+        // m, and s2 of size n
+
+        // first row and first column will be 0 since, agar string ka size==0
+        // hai toh kisse compare karoge
+
+        // pehle column ko 0 kardo
+        for (int row = 0; row < m + 1; row++) {
+            t[row][0] = 0;
+        }
+        // pehle row ko 0 kardo
+        for (int col = 0; col < n + 1; col++) {
+            t[0][col] = 0;
         }
 
-        // nai toh 2 options hai
-        // option1-> i ko aag badhao, and j ko same rakho
-        // option1-> j ko aag badhao, and i ko same rakho
+        // ab iss tabulation ko bharo
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                // curr banda is i,j
 
-        // dono me se jo bhi maaxm dega vohi ans hoga
+                // toh agar i-1 th char of s1 same as j=1 th char of s2
+                if (s1[i - 1] == s2[j - 1]) {
+                    // means char same hai
 
-        // memo me update karke bhejna
-        memo[i][j]=max(solve(s1,s2,i+1,j,memo),solve(s1,s2,i,j+1,memo));
-        return memo[i][j];
-    }
-    int longestCommonSubsequence(string s1, string s2) {
-        m=s1.size();
-        n=s2.size();
+                    // next wale par jao, toh usially we wouldve done
+                    // t[i+1][j+1] lekin yaha apan peeche se chalu krre hai toh
+                    // ans aage se nai nikal sakte(since vo solved hi nai h
+                    // abhi)
 
-        // TLE, memo banao
-    vector<vector<int>> memo(m,vector<int>(n,-1));
+                    // toh instead ans nikalo peeche waale se
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } else {
+                    // means 2 options hai, ya toh i-1,j jao, ya toh i,j-1 jao
 
-        return solve(s1,s2,0,0,memo);
+                    // dono me se jo maxm ans de vo lelo
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+
+        return t[m][n];
     }
 };
