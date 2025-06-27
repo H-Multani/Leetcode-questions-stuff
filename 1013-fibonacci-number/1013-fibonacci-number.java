2aa8532733868fefc7718ea7566d23ae83ee1431
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-06-27 12:44:38 +0530

    Time: 0 ms (100%), Space: 41 MB (6.79%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,43 @@
 class Solution {
-    public int fib(int n) {
-        if(n<=1) return n;
+    public static int[][] multiply(int[][] a,int[][] b){
+        int[][] c=new int[2][2];
 
-        return fib(n-1)+fib(n-2);
+        c[0][0]=(a[0][0]*b[0][0])+(a[0][1]*b[1][0]);
+        c[0][1]=(a[0][0]*b[0][1])+(a[0][1]*b[1][1]);
+        c[1][0]=(a[1][0]*b[0][0])+(a[1][1]*b[1][0]);
+        c[1][1]=(a[1][0]*b[0][1])+(a[1][1]*b[1][1]);
+        return c;
+    }
+    public static int[][] pow(int[][] M, int exp){
+        if(exp==0){
+            // return identity matrix
+            int[][] I={{1,0},{0,1}};
+            return I;
+        }
+        int[][] half=pow(M,exp/2);
+        int[][] res=multiply(half,half);
+
+
+        if(exp%2==1){
+            res=multiply(M,res);
+        }
+
+        return res;
+
+    }
+    public static int solvematrix(int n){
+
+        if(n==0 || n==1) return n;
+
+        int[][] T={{1,1},{1,0}};
+        int[][] F={{1,0},{0,0}};
+        int[][] res=pow(T,n-1);
+
+        res= multiply(res,F);
+
+        return res[0][0];
+    }
+    public int fib(int n) {
+        return solvematrix(n);
     }
 }
