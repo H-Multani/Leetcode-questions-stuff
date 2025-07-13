8acf01d62d6c5944b104a7092638a2824200ec38
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-07-13 15:35:10 +0530

    Time: 35 ms (15.22%), Space: 80.5 MB (5.47%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& g, vector<int>& s) {
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-        vector<bool> gbool(g.size(),false);
-        int gi=0,si=0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        vector<bool> gbool(g.size(), false);
+        int gi = 0, si = 0;
 
-        while(gi<g.size() && si<s.size()){
-            if(g[gi]<=s[si]){
-                gbool[gi]=true;
+        // current player ko konsa wala trainer satisfy kar payega
+
+        // toh iterate over the trainers, match trainers with corresposning
+        // student, agar current trainer inadequate, move to next
+        // trainer,otherwise if adequate, move to next trainer and the next
+        // student
+
+        while (gi < g.size() && si < s.size()) {
+            if (g[gi] <= s[si]) {
+                gbool[gi] = true;
                 gi++;
                 si++;
-            }
-            else{
+            } else {
                 si++;
             }
         }
 
-        int truecnt=0;
+        int truecnt = 0;
 
-        for(auto it:gbool){
-            if(it) truecnt++;
+        // count kitne students satisfied till here
+        for (auto it : gbool) {
+            if (it)
+                truecnt++;
         }
 
         return truecnt;
