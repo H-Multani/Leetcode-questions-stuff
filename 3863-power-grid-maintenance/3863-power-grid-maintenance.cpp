0d5a1a02eaed0f482e174ec5292d1e1c8ddee3e5
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2025-11-07 02:14:55 +0530

    Time: 428 ms (43.66%), Space: 393 MB (46.28%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
@@ -32,9 +32,9 @@ public:
     }
 
     // update function bana do ek, which will keep the parent array updated
-    void update(){
-        for(int i=0;i<parent.size();i++){
-            parent[i]=find(parent[i]);
+    void update() {
+        for (int i = 0; i < parent.size(); i++) {
+            parent[i] = find(parent[i]);
         }
 
         return;
@@ -50,7 +50,7 @@ public:
         unordered_set<int> down;
 
         // dsu bana do c+1 size ka since we need 1 based indexing
-        DSU grid(c+1) ;
+        DSU grid(c + 1);
 
         // ab connect kardo connections me iterate karke
         for (auto it : connections) {
@@ -60,25 +60,24 @@ public:
         // saare connections done and saare grids made now
 
         // abb apan ek map with set lenge which will store ki har grid par konsa
-        // element hai like ig towers 2,4,5,1 are on grid 2, then map me key
+        // element hai like if towers 2,4,5,1 are on grid 2, then map me key
         // will be 2, value will be {1,2,4,5} isse apan [1,x] wali queries ke
         // liye smallest id jaldi nikal payenge
 
-        unordered_map<int,set<int>> towers;
+        unordered_map<int, set<int>> towers;
 
-        // update kardo sabke super parents
+        // update kardo sabke super parents nai toh ho sakta hai galat parent ke
+        // set me daal de tower ko
         grid.update();
 
         // iterate over dsu.parent
 
         // start frm 1 since 0 wala tower akela hai not needed
-        for(int i=1;i<grid.parent.size();i++){
+        for (int i = 1; i < grid.parent.size(); i++) {
             // current tower is i
 
             // current tower konse grid ka hai ye uska parent batayega
-
-            // super parent nikal lo best
-            int parentt=grid.parent[i];
+            int parentt = grid.parent[i];
 
             // iss parent ke set me current tower ke id ko daal do
             towers[parentt].insert(i);
@@ -87,49 +86,51 @@ public:
         // ab apna kaam mostly done hai query par iterate karo bass
 
         vector<int> ans;
-        for(auto it:queries){
+        for (auto it : queries) {
             // agar type 2 hai toh
-            if(it[0]==2){
+            if (it[0] == 2) {
                 // means x wala tower down karo
-                int x=it[1];
+                int x = it[1];
 
                 // sabse pehle ye tower konse grid me hai vo niklo
-                int parentt=grid.parent[x];
+                int parentt = grid.parent[x];
 
                 // map me se iss bande ko remove krdo
                 towers[parentt].erase(x);
 
                 // kaam khatam
-            }
-            else{
-                // print parents of each 
+            } else {
+                // print parents of each
                 // for(int i=1;i<grid.parent.size();i++){
                 //     cout<<i<<" ka parent "<<grid.parent[i]<<endl;
                 // }
                 // cout<<endl;
 
                 // we here means type 1 hai
-                int x=it[1];
+                int x = it[1];
 
                 // pehle iss x ka parent nikalo
-                int parentt=grid.parent[x];
+                int parentt = grid.parent[x];
 
                 // ab check karo if ye availale hai ki nai map me
-                if(towers[parentt].find(x)==towers[parentt].end()){
-                    // means nai hai, in such case smallest id nikal lo
+                if (towers[parentt].find(x) == towers[parentt].end()) {
+                    // means nai hai, in such case smallest id nikal lo same
+                    // grid se
 
                     // agar set empty hai toh -1 ans me daal do
-                    if(towers[parentt].size()==0){
+                    if (towers[parentt].size() == 0) {
                         ans.push_back(-1);
                         continue;
                     }
-                    int id=*towers[parentt].begin();
+
+                    // set hai toh smallest id at beginning hogi 
+                    int id = *towers[parentt].begin();
 
                     // iss wali id ko ans me daal do
                     ans.push_back(id);
-                }
-                else{
-                    // we here means ki tower is available, in such case khud hi manage kar lega tower
+                } else {
+                    // we here means ki tower is available, in such case khud hi
+                    // manage kar lega tower
                     ans.push_back(x);
                 }
             }
