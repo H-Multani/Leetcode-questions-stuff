bcc702eab19ca9371d27e67afca46730da01fb58
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2026-02-07 02:28:36 +0530

    Time: 34 ms (50%), Space: 105 MB (52.63%) - LeetHub

---
+++ 3958-minimum-removals-to-balance-array/3958-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        // simple sliding window, hints hi bata diye
+
+        // sort the array first
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        int i = 0, j = 0, ans = -1;
+
+        // i wala element is minm, and j wala element is maxm
+
+        while (j < n) {
+            // current wala element andar lelo
+
+            // yaha kuch hai nai as such karne ke liye
+
+            // minm element is nums[i] and maxm element is nums[j]
+            // agar nums[j]<=nums[i]*k, means apan windo badha sakte hai aage,
+            // since question wala order follow hora, otherwise window ko shrink
+            // karo
+            while (i < n && (1LL*nums[j]) > (1LL*nums[i] * k)) {
+                // means we need to shrink window since maxm has become greater
+                // than minm * k
+
+                // i++ kardo bass
+                i++;
+            }
+
+            // we here means apan ke paas ek valid subarray hai which contains
+            // all elements such that minm*k>=maxm
+
+            // ye length ek possible ans hai update kardo
+            ans=max(ans,j-i+1);
+
+            // extend window
+            j++;
+        }
+
+
+        // ab ans is the length of biggest valid array, means baaki elements nikalne padenge array se, toh overall ans is n-ans, since n-ans is the no of elements we have to remove to get to that perfect array
+        return n-ans;
+    }
+};
