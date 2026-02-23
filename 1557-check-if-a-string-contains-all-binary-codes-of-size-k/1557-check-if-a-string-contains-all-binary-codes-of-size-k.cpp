9ac3e27684c2718a3a7831272a34cca5af5eeb88
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2026-02-24 02:15:42 +0530

    Time: 545 ms (27.83%), Space: 150.5 MB (26.21%) - LeetHub

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -7,13 +7,16 @@ public:
         // substring ka def bolta ki beech se element nikal nahi sakte , toh k
         // size ka substring banate jao(from s) set me daalte jao, agar end me
         // 2^k unique substrings bane toh means saare possisble binary
-        // substrings ban gaye hai otherwise ans cannot be made
+        // substrings ban gaye hai otherwise ans cannot be made, since 2^k se
+        // zyada binary substrings possible hi nahi hai of length k, toh agar
+        // ban gaye matlab s ke substrings se k length ke saare binary strings
+        // bana sakte hai
 
         unordered_set<string> subs;
 
         // starting frm idx 0 saare substrings nikalo of k size each
         int n = s.size();
-        for (int i = 0; i < n - k+1; i++) {
+        for (int i = 0; i < n - k + 1; i++) {
             // substring nikalo of k size
             string substring = s.substr(i, k);
 
@@ -22,11 +25,25 @@ public:
         }
 
         // agar subs ka size is 2^k means all possible binary strings of length
-        // k can be made out of string s, in such case reurn true
+        // k can be made out of string s, in such case return true
 
-        if (subs.size() >= (1 << k))
+        // this works since k size ke total 2^k binary strings ban sakte hai, ab
+        // apan agar k size ka har possible substring nikal kar rakh le from s
+        // toh ye hoga ki apne paas k length ke 2^k unique substrings available
+        // hai, that can only mean one thing ki string s can be used to make all
+        // possible substrings of length k, sinnce maxm possible 2^k strings ban
+        // sakte the of k size, aur apan ne same 2^k unique substrings bana diye
+        // hai from all k length substrings of s, toh that means all possible
+        // strings of length k can be made from substrings of s, in such case
+        // return true
+
+        // 2^k karne ka ez tareeka 1<<k, ie 1 leftshift k times,
+        if (subs.size() == (1 << k))
             return true;
 
+        // we here means subs me kam elements hai ie all possible binary strings
+        // of k size cannot be made by substrings of s, in such case return
+        // false
         return false;
     }
 };
