a218a1c641c7e0b3563e6ef5f4f96b803c303b89
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2026-05-09 03:17:38 +0530

    Time: 642 ms (62.87%), Space: 280.9 MB (79.05%) - LeetHub

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/3933-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/3933-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -30,7 +30,7 @@ public:
         int n = nums.size();
 
         // use a map which will store element and indeices, will help when
-        // multiple times same element aaye
+        // multiple times same element aaye, toh q me daalne me easier hoga
         unordered_map<int, vector<int>> mpp;
         for (int i = 0; i < n; i++) {
             mpp[nums[i]].push_back(i);
@@ -39,10 +39,14 @@ public:
         // ek set le lenge which will store used prime nos, ye prime nos ko
         // already use kar chuke hai these wont be needed again
         unordered_set<int> seen;
+        // logic is ek baari koi prime par aaye toh uske saare options ek baar
+        // me q me daal denge, sab explore ho jaenge, toh apan ko baar baar same
+        // exploreation nai karna padega
 
         // visited array bana do
         vector<bool> vis(n + 1, false);
-        // q bana do dor BFS
+
+        // q bana do for BFS
         queue<int> q;
         // this q stores indexes of places visited
 
@@ -69,7 +73,7 @@ public:
                 q.pop();
 
                 // agar galti se last element par pahuch gaye toh return kitne
-                // steps aaye hai apan frm her
+                // steps aaye hai apan till here
                 if (curridx == n - 1)
                     return steps;
 
@@ -78,6 +82,7 @@ public:
                 // curridx se apan ke paas 2 options hai,
 
                 // option 1,adjacent par jao
+
                 // i+1 par jao
                 // agar i+1 is already visited then q me mat dalna
                 if (curridx + 1 < n && !vis[curridx + 1]) {
@@ -105,7 +110,8 @@ public:
                     // nai kiya hai
 
                     // iss prime ke har multiple ko nums me dhundo and q me daal
-                    // do as possible areas to explroe
+                    // do as possible areas to explore, for this we will use the
+                    // map above
 
                     for (int j = curr; j <= maxm; j += curr) {
                         // means j is multiple of prime no,
@@ -130,7 +136,8 @@ public:
                     }
 
                     // ab current prime ke liye saare paths explore kar liye,
-                    // set me daal do so that baad me first explore na ho
+                    // set me daal do so that baad me fir se explore na ho yehi
+                    // same prime
                     seen.insert(curr);
                 }
             }
