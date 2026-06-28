8fecebe8982f59c650a24d1370a1a6aa4d905d41
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2026-06-28 05:49:42 +0530

    Time: 16 ms (7.34%), Space: 55.1 MB (69.21%) - LeetHub

--- 1956-maximum-element-after-decreasing-and-rearranging/1956-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/1956-maximum-element-after-decreasing-and-rearranging.cpp
@@ -13,10 +13,10 @@ public:
         int n=arr.size();
         for(int i=1;i<n;i++){
             if(abs(arr[i]-arr[i-1])<=1){
-                // sahi hai aage badho
+                // sahi hai aage badho, no need to change anything here, diff is <=1, chah kar bhi arr[i] ko aage nai badha sakte since only reduce karne ka option hai apne paas
             }
             else{
-                // galat hai, this means arr[i] is boht bigger than arr[i-1], in this case, arr[i] ko arr[i-1]+1 par laa kar chhor do, so that biggest ans tak badh bhi paaye
+                // galat hai, this means arr[i] is boht bigger than arr[i-1], in this case, arr[i] ko arr[i-1]+1 par laa kar chhor do, so that biggest ans tak badh bhi paaye and valid bhi rahe
                 arr[i]=arr[i-1]+1;
             }
         }
