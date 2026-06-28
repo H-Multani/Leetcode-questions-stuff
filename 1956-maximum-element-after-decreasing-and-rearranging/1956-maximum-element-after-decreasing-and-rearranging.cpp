a7f48a66a29ebfaffcad003bb1f04f76b7f3524d
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2026-06-28 05:48:20 +0530

    Time: 16 ms (7.34%), Space: 55.1 MB (94.35%) - LeetHub

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/1956-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        // sabse pehle toh sort karna padega
+        sort(arr.begin(),arr.end());
+
+        // ab agar start par 1 already hai to baaki elements order me karenge(nai hai toh), otherwise aage badhte jaenge
+
+        // thing to note is apan element ko bas kam kar sakte hai aage nai badha sakte, need to keep that in mind
+
+        if(arr[0]!=1) arr[0]=1;
+
+        int n=arr.size();
+        for(int i=1;i<n;i++){
+            if(abs(arr[i]-arr[i-1])<=1){
+                // sahi hai aage badho
+            }
+            else{
+                // galat hai, this means arr[i] is boht bigger than arr[i-1], in this case, arr[i] ko arr[i-1]+1 par laa kar chhor do, so that biggest ans tak badh bhi paaye
+                arr[i]=arr[i-1]+1;
+            }
+        }
+
+        return arr[n-1];
+    }
+};
