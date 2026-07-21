d32310b8aacebc48ea822fc82b0a12b3c28267e7
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2026-07-21 18:29:27 +0530

    Time: 69 ms (53.6%), Space: 80.8 MB (36.04%) - LeetHub

---
+++ 3805-maximize-active-section-with-trade-i/3805-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        // count only bundles of 0s
+        vector<int> arr;
+        int cnt = 0;
+        int onecnt = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0')
+                cnt++;
+            else {
+                // one aaya hai active section hai cnt karlo
+                onecnt++;
+                // count agar non 0 hai toh arr me push kardo
+                if (cnt != 0) {
+                    arr.push_back(cnt);
+                }
+
+                // reset cnt to 0
+                cnt = 0;
+            }
+        }
+        // count agar non 0 hai toh arr me push kardo
+        if (cnt != 0) {
+            arr.push_back(cnt);
+        }
+
+        // now we have half ans made already, which is cnt of 1s
+
+        // question bolta ki 0s ke beech wale 1s ke bundle ko 0 bana do and fir
+        // uss bade 0s ke contious block ko 1 bana do, toh beech wale 1s end me
+        // 1s hi rahenge, and aaju baaju wale 0s ke block 1s ban jaenge
+
+        // toh 100110001 becomes->100000001-> 111111111,
+
+        // basically 2 adjacent blocks of 0s become 1s, and get added to ans
+
+        //  toh we just need maxm sum of 2 adjacent blocks of 0s, nikal kar add
+        //  kardo
+        int m = arr.size();
+        int ans = onecnt;
+        // jitne 1s already hai utne count me include kardo yehi par
+
+        // iterate through 0s ke blocks and get maxm sum of 2 adjacent blocks
+        // and update ans
+        for (int i = 1; i < m; i++) {
+            // current and peeche wale ka pair check karo accha ans banega kya
+
+            // banega toh update kardena,
+            ans = max(ans, onecnt + (arr[i] + arr[i - 1]));
+        }
+
+        return ans;
+    }
+};
