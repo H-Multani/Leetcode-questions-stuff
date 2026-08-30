f3a4f9f81c7672e404103d257a66960e714897d9
Harsh Multani <82038303+H-Multani@users.noreply.github.com>
2026-08-31 01:39:18 +0530

    Time: 0 ms (100%), Space: 92.1 MB (22.32%) - LeetHub

--- 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.cpp
@@ -5,7 +5,7 @@ public:
         int mini = min_element(nums.begin(), nums.end()) - nums.begin();
         int maxi = max_element(nums.begin(), nums.end()) - nums.begin();
 
-        cout << mini << " " << maxi << endl;
+        // cout << mini << " " << maxi << endl;
         int n = nums.size();
         int ans = n + 1;
 
@@ -17,7 +17,7 @@ public:
         // since maan lo min is at idx 5 and max is at idx 7, toh left se 8
         // elements nikalne padenge so that both min and max remove ho sake
         ans = min(ans, max(mini + 1, maxi + 1));
-        cout<<"ans is "<<ans<<endl;
+        // cout<<"ans is "<<ans<<endl;
 
         // similarly
         // case 2-> saare elements back side se nikal lo, uss case me we need to
@@ -27,7 +27,7 @@ public:
         // right se 5 elements nikalne padenge so that both min and max remove
         // ho sake
         ans = min(ans, max(n - mini, n - maxi));
-        cout<<"ans is "<<ans<<endl;
+        // cout<<"ans is "<<ans<<endl;
 
         // case 3-> mini wala right se nikal liye, maxi wala left se nikal liye,
         // and iska vice versa
@@ -38,7 +38,7 @@ public:
         // min(maxi+1,n-maxi)
 
         ans=min(ans,min(mini+1,n-mini)+min(maxi+1,n-maxi));
-        cout<<"ans is "<<ans<<endl;
+        // cout<<"ans is "<<ans<<endl;
 
         return ans;
     }
